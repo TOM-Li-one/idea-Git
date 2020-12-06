bc88afd196cbc966f1310e1bc8bce5b93c667865
Tom_global <Tom_global@qq.com>
2020-12-07 01:00:14 +0800

    把本地工程添加到本地库

---
+++ firstGit.project/src/com/atguigu/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author LIJUN
+ * @create 2020-12-07-0:46
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("wo是一个啥也不会的研究生");
+    }
+}
