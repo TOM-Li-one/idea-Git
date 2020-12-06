7c2753d5b4a959984a0018d59eaa2e0cfcc0cb7d
Tom_global <Tom_global@qq.com>
2020-12-07 01:01:55 +0800

    把本地工程添加到本地库

--- firstGit.project/src/com/atguigu/git/FirstClass.java
+++ firstGit.project/src/com/atguigu/git/FirstClass.java
@@ -8,4 +8,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("wo是一个啥也不会的研究生");
     }
+    public void test(){
+        int i =10;
+        int s = i +1;
+        System.out.println(s);
+    }
 }
